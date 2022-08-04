75c5c5f574deb7bf340efec10d9717f29d80e25b
CMiller <mlr.chase40@gmail.com>
2022-08-04 15:56:43 -0500

    add pwa download capabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const webpack = require('webpack');
+const M = require('minimatch');
 
 module.exports = {
   entry: {
@@ -42,6 +44,23 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static'
+    }),
+    new WebpackPwaManifest({
+      name: 'Food Event',
+      short_name: 'Foodies',
+      description: 'An app that allows you to view upcoming food events.',
+      start_url: '../index.html',
+      background_color: '#01279b',
+      theme_color: '#ffffff',
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve('assets/img/icons/icon-512x512.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons')
+        }
+      ]
     })
   ],
   mode: 'development'
